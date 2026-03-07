f278c2d0ba74779b74d58f659f535d690fabcd8a
Terence Le Thierry <terence.lethierry@protonmail.com>
2026-03-07 13:22:58 +0100

    Added a check_structure_validity() function

--- user_data/circuit_files.py
+++ user_data/circuit_files.py
@@ -6,6 +6,29 @@ import user_data.sanitize_filenames
 import user_data.circuit
 
 
+
+def check_structure_validity(json_file:str) -> bool:
+    """Returns True if the given JSON file has a valid data structure for a Circuit object, otherwise returns False."""
+
+    root_key = "data" # Expected root key for the JSON structure
+
+    keys = ["metadata"] # Expected keys inside the JSON structure
+
+    # Load the JSON file
+    with open(json_file, "r", encoding="utf-8") as f:
+        json_data = json.load(f)
+
+
+    # Check if the root key is not present
+    if not root_key in json_data:
+        return False
+    
+    json_structure = json_data[root_key] # Get the JSON structure contained by the root key
+    if not any([key in json_structure] for key in keys):
+        return False
+    
+    return True
+
 def open_circuit(file_path:str) -> dict:
     """Opens the given JSON circuit file and returns the dictionnary describing that circuit."""
 
